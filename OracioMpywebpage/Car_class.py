d312a011ea3f712732059c2d11e7b4798d1ad0b8
CameronTran71 <camerontran796@yahoo.com>
2025-10-07 15:46:43 -0700

    Create Car_class.py
    class for representing the car information

---
+++ OracioMpywebpage/Car_class.py
@@ -0,0 +1,14 @@
+class Car:
+    """Class to represent a car in the rental system"""
+    
+    def __init__(self, car_id, make, model, year, car_type, is_available=True):
+        self.car_id = car_id
+        self.make = make
+        self.model = model
+        self.year = year
+        self.car_type = car_type  # e.g., 'SUV', 'Sedan', 'Truck'
+        self.is_available = is_available
+    
+    def __str__(self):
+        status = "Available" if self.is_available else "Rented"
+        return f"{self.year} {self.make} {self.model} ({self.car_type}) - {status}"
